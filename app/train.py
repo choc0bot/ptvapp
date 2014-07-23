057a036b2803beff94cbecd152b78f3ae130ab9c
choc0bot <choc0bot@gmail.com>
2014-07-23 14:26:28 +1000

    added home and work

--- app/train.py
+++ app/train.py
@@ -3,6 +3,10 @@ import ptvapi
 import calendar
 from datetime import datetime, timedelta
 
+work_train = (1155,14)
+home_train = (1014,0)
+
+
 def utc_to_local(utc_dt):
     # get integer timestamp to avoid precision lost
     timestamp = calendar.timegm(utc_dt.timetuple())
@@ -14,7 +18,18 @@ def timestring(mytime):
 	mytimestr = mytime.strftime('%H:%M')
 	return mytimestr
 
+def get_train_time(train_tuple):
+	mydep = ptvapi.specificNextDepartures(0,15,train_tuple[0],train_tuple[1],1)
+	train_list = mydep[u'values']
+	first = train_list[0]
+	if len(train_list)>1:
+		first = train_list[1]
+	mytime = first[u'time_timetable_utc']
+	datestr = datetime.strptime( mytime[:-1], "%Y-%m-%dT%H:%M:%S" )
+	mytime = utc_to_local(datestr)
+	return timestring(mytime)
 
+"""
 def next_upfield():
 	mydep = ptvapi.specificNextDepartures(0,15,1155,14,1)
 	train_list = mydep[u'values']
@@ -37,9 +52,16 @@ def next_batman():
 	mytime = utc_to_local(datestr)
 	return timestring(mytime)
 
+"""
+
 def c_time():
 	#curtime = datetime.now().time()
 	curtime = datetime.now().strftime('%H:%M')
 	return curtime
 
+
 #print c_time()
+
+#print get_train_time(work_train)
+
+#print get_train_time(home_train)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from train import next_upfield, c_time, next_batman
+from train import get_train_time, c_time
 
 @app.route('/')
 @app.route('/index')
@@ -13,11 +13,13 @@ def index():
 
 @app.route('/train')
 def train():
+	work_train = (1155,14)
+	home_train = (1014,0)
 	curtime = c_time()
-	battrain = next_batman()
-	uptrain = next_upfield()
+	battrain = get_train_time(home_train)
+	parltrain = get_train_time(work_train)
 	return render_template("train.html",
     	title = 'Train',
-    	uptrain = uptrain,
+    	parltrain = parltrain,
     	battrain = battrain,
     	curtime = curtime)
